f982eb54d8a8ba6c89ffaea103c37e7d35519eb8
Sushmitha Popuri <sushmithapopuri@outlook.com>
2016-08-15 10:16:10 +0530

    v4

--- src/main/java/dao/DbConnect.java
+++ src/main/java/dao/DbConnect.java
@@ -71,7 +71,7 @@ public class DbConnect {
 			httpsURLConnection.setDoOutput(true);
 			httpsURLConnection.addRequestProperty("User-Agent",
 					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-			return true;
+			return httpsURLConnection.getResponseMessage()=="true";
 		}catch(Exception exception){
 
 		}
