f64a6d3d08ff6ab80f05ed645a01bcc7222aef35
Sushmitha Popuri <sushmithapopuri@outlook.com>
2016-08-15 10:06:38 +0530

    v3

--- src/main/java/resources/HelloResource.java
+++ src/main/java/resources/HelloResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -20,17 +21,19 @@ public class HelloResource {
     }
     @POST
 	@Path("/addnewuser")
-	@Consumes("application/json")
 	@Produces("text/plain")
-	public static String signUp(User user){
-//	User user=new User();
-    	System.out.println("input is : "+user.getAuthId());
-	user.setAuthId("2423");
-	user.setEmail("user1@api.com");
-	user.setName("user1");
-	user.setDetails("Test1");
-	user.setPhoneNumber("1234567890");
-	user.setOrganization("org1");
+	public static String signUp(@PathParam("name") String username,@PathParam("authId")String authId,@PathParam("phNo")String nubmer){
+	User user=new User();
+	user.setAuthId(authId);
+    user.setName(username);
+    user.setPhoneNumber(nubmer);
+	System.out.println("input is : "+user.getAuthId());
+//	user.setAuthId("2423");
+//	user.setEmail("user1@api.com");
+//	user.setName("user1");
+//	user.setDetails("Test1");
+//	user.setPhoneNumber("1234567890");
+//	user.setOrganization("org1");
 	String status= UserData.registerUser(user);
 	System.out.println("The status is "+status);
 	return status;
