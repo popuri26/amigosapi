d23c1004583a4680c5c9a1e8e131629a26deb354
Sushmitha Popuri <sushmithapopuri@outlook.com>
2016-08-22 22:31:26 +0530

    v23

--- src/main/java/resources/HelloResource.java
+++ src/main/java/resources/HelloResource.java
@@ -21,8 +21,9 @@ public class HelloResource {
 	@POST
 	@Path("/addnewuser")
 	@Produces("text/plain")
-	public static String signUp(@QueryParam("name") String username,@QueryParam("auth_id") String authId,@QueryParam("phNo")String nubmer,
-			@QueryParam("email") String email,@QueryParam("details") String details,@QueryParam("org") String organization,
+	public static String signUp(@QueryParam("name") String username,@QueryParam("auth_id") String authId,
+			@QueryParam("phNo")String nubmer,@QueryParam("email") String email,@QueryParam("details") String details,
+			@QueryParam("org") String organization,@QueryParam("credit") String credit,
 			@QueryParam("token") String token,@QueryParam("secret")String secret,@QueryParam("pin")String pin){
 		User user=new User();
 		user.setAuthId(authId);
